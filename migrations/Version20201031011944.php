ce5979d5c8fb6d0860bab578f5d1094523348fee
Sergio Gómez <sergio@uco.es>
2020-10-31 13:02:37 +0100

    Add 2fa fields to User

---
+++ migrations/Version20201031011944.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201031011944 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD email VARCHAR(180) NOT NULL, ADD googleAuthenticatorSecret VARCHAR(255) DEFAULT NULL, ADD googleActivationSecret VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user DROP email, DROP googleAuthenticatorSecret, DROP googleActivationSecret');
+    }
+}
