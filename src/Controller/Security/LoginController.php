52daf208e4c29928722512bc111d0e51133610e3
Sergio Gómez <sergio@uco.es>
2020-10-31 13:06:26 +0100

    Add 2fa controllers

---
+++ src/Controller/Security/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        if ($this->getUser() instanceof UserInterface) {
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'page_title' => 'Iniciar sesión en LOCK',
+        ]);
+    }
+}
