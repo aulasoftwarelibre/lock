55f1259eac28399de4704e1cc6694ada94197fd7
Sergio Gómez <sergio@uco.es>
2021-04-03 22:28:23 +0200

    feat: psalm fixes

--- src/Security/Checker/TwoFactorChecker.php
+++ src/Security/Checker/TwoFactorChecker.php
@@ -1,11 +1,10 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Security\Checker;
 
-
 use App\Entity\User;
-use Symfony\Component\Security\Core\Exception\AccountStatusException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -17,11 +16,11 @@ final class TwoFactorChecker implements UserCheckerInterface
      */
     public function checkPreAuth(UserInterface $user)
     {
-        if (!$user instanceof User) {
+        if (! $user instanceof User) {
             return;
         }
 
-        if (!$user->isGoogleAuthenticatorActivated()) {
+        if (! $user->isGoogleAuthenticatorActivated()) {
             throw new CustomUserMessageAccountStatusException('No has activado el código de la aplicación.');
         }
     }
