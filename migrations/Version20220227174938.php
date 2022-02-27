87d8ca973992fb62ddf954ddf238708aafc600ca
Sergio Gómez <decano@gmail.com>
2022-02-27 19:22:20 +0100

    feat: upload qr codes to secrets

---
+++ migrations/Version20220227174938.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220227174938 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE secret ADD updated_at DATETIME NOT NULL, ADD image_name VARCHAR(255) DEFAULT NULL, ADD image_original_name VARCHAR(255) DEFAULT NULL, ADD image_mime_type VARCHAR(255) DEFAULT NULL, ADD image_size INT DEFAULT NULL, ADD image_dimensions LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:simple_array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE secret DROP updated_at, DROP image_name, DROP image_original_name, DROP image_mime_type, DROP image_size, DROP image_dimensions');
+    }
+}
--- src/Entity/Secret.php
+++ src/Entity/Secret.php
@@ -14,10 +14,12 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[UniqueEntity(fields: ['site', 'account'])]
 #[ORM\Entity(repositoryClass: SecretRepository::class)]
- *
+#[Vich\Uploadable]
 class Secret
 {
     #[ORM\Id]
@@ -43,16 +45,37 @@ class Secret
     /** @var Collection<int, OrganizationUnit> */
     #[ORM\ManyToMany(targetEntity: OrganizationUnit::class)]
     private Collection $organizations;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?DateTimeInterface $updatedAt;
+
+
     /**
-     * @ORM\ManyToMany(targetEntity=OrganizationUnit::class)
-     *
-     * @var Collection<int, OrganizationUnit>
+     * @Vich\UploadableField(
+     *     mapping="avatars",
+     *     fileNameProperty="image.name",
+     *     size="image.size",
+     *     mimeType="image.mimeType",
+     *     originalName="image.originalName"
+     * )
      */
-    private Collection $organizations;
+    #[Vich\UploadableField(
+        mapping: 'codes',
+        fileNameProperty: 'image.name',
+        size: 'image.size',
+        mimeType: 'image.mimeType',
+        originalName: 'image.originalName',
+    )]
+    private File|UploadedFile|null $imageFile = null;
+
+    #[ORM\Embedded(class: 'Vich\UploaderBundle\Entity\File')]
+    private EmbeddedFile $image;
 
     public function __construct()
     {
         $this->organizations = new ArrayCollection();
+        $this->image         = new EmbeddedFile();
+        $this->updatedAt     = new DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -118,5 +141,42 @@ class Secret
         $this->organizations->removeElement($organization);
 
         return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     */
+    public function setImageFile(?File $image = null): void
+    {
+        $this->imageFile = $image;
+
+        if ($image === null) {
+            return;
+        }
+
+        // It is required that at least one field changes if you are using doctrine
+        // otherwise the event listeners won't be called and the file is lost
+        $this->updatedAt = new DateTimeImmutable();
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage(EmbeddedFile $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getImage(): EmbeddedFile
+    {
+        return $this->image;
     }
 }
