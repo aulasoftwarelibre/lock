b6373fc886809e4005611ecd7a1be9af008a4d6e
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-05 22:06:56 +0100

    feat: removing old symphony secrets to start over

--- config/secrets/prod/prod.list.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'APP_SECRET' => null,
-    'CLIENT_UCO_ID' => null,
-    'CLIENT_UCO_SECRET' => null,
-    'DATABASE_URL' => null,
-    'MAILER_DSN' => null,
-];
--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -3,47 +3,47 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
+  set -- php-fpm "$@"
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
-	if [ "$APP_ENV" != 'prod' ]; then
-		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
-	fi
-	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
-
-	mkdir -p var/cache var/log public/cache public/images
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var public
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var public
-
-  if [ -z "$SYMFONY_DECRYPTION_SECRET" ]; then
-      echo "Define SYMFONY_DECRYPTION_SECRET to decrypt secrets"
-      exit 1
+  PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
+  if [ "$APP_ENV" != 'prod' ]; then
+    PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
   fi
+  ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+
+  mkdir -p var/cache var/log public/cache public/images
+  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var public
+  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var public
+
+  #if [ -z "$SYMFONY_DECRYPTION_SECRET" ]; then
+  echo "Define SYMFONY_DECRYPTION_SECRET to decrypt secrets"
+  #   exit 1
+  #fi
 
   bin/console secrets:decrypt-to-local --force --env=prod
 
-	echo "Waiting for db to be ready..."
-	ATTEMPTS_LEFT_TO_REACH_DATABASE=60
-	until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
-		sleep 1
-		ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE-1))
-		echo "Still waiting for db to be ready... Or maybe the db is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left"
-	done
+  echo "Waiting for db to be ready..."
+  ATTEMPTS_LEFT_TO_REACH_DATABASE=60
+  until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || bin/console doctrine:query:sql "SELECT 1" >/dev/null 2>&1; do
+    sleep 1
+    ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+    echo "Still waiting for db to be ready... Or maybe the db is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left"
+  done
 
-	if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
-		echo "The db is not up or not reachable"
-		exit 1
-	else
-	   echo "The db is now ready and reachable"
-	fi
+  if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+    echo "The db is not up or not reachable"
+    exit 1
+  else
+    echo "The db is now ready and reachable"
+  fi
 
-	if ls -A migrations/*.php > /dev/null 2>&1; then
-		bin/console doctrine:migrations:migrate --no-interaction
-	fi
+  if ls -A migrations/*.php >/dev/null 2>&1; then
+    bin/console doctrine:migrations:migrate --no-interaction
+  fi
 
-    bin/console cache:warmup --env=prod
+  bin/console cache:warmup --env=prod
 fi
 
 exec docker-php-entrypoint "$@"
